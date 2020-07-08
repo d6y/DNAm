38549eca72183f2aa09de8a0aa80e1253e50b652
Richard Dallaway <richard@dallaway.com>
2020-07-08 17:46:46 +0100

    interate once through CSV data

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-type AppErr = Box<dyn std::error::Error>;
-
 const HORVATH: &[u8; 9577] = include_bytes!("../coefficients/CpG-coefficients.csv");
-
 const PHENO: &[u8; 12524] = include_bytes!("../coefficients/coef_pheno.csv");
 
 struct Model {
+    name: String,
     coefs: HashMap<String, f32>,
     adjust: fn(f32) -> f32,
 }
@@ -24,42 +22,55 @@ fn main() -> Result<(), AppErr> {
     let config = Config::from_args();
 
     let h = Model {
+        name: String::from("Horvath"),
         coefs: load_coefficients(HORVATH)?,
-        adjust: |m| m.exp() * 21.0 - 1.0
+        adjust: |m| m.exp() * 21.0 - 1.0,
     };
 
     let p = Model {
+        name: String::from("Phenotype"),
         coefs: load_coefficients(PHENO)?,
-        adjust: std::convert::identity
+        adjust: std::convert::identity,
     };
 
-    let h_age = apply(&h, &config.file)?;
-    println!("horvath:   {:.2}", h_age);
+    let models = vec![&h, &p];
 
-    let p_age = apply(&p, &config.file)?;
-    println!("phenotype: {:.2}", p_age);
+    let ages = apply(&models, &config.file)?;
+
+    for (&model, age) in models.iter().zip(ages) {
+        println!("{:9} : {:.2} years", model.name, age);
+    }
 
     Ok(())
 }
 
-fn apply(model: &Model, file: &PathBuf) -> Result<f32, AppErr> {
+fn apply(models: &[&Model], file: &PathBuf) -> Result<Vec<f32>, AppErr> {
     let mut rdr = csv::Reader::from_path(file)?;
 
-    let mut age: f32 = *model.coefs.get("intercept").expect("intercept not found");
+    let mut ages: Vec<f32> = Vec::with_capacity(models.len());
+
+    for model in models {
+        ages.push(*model.coefs.get("intercept").expect("intercept not found"));
+    }
 
     for result in rdr.records() {
         let record = result?;
         let key = record[0].to_owned();
-        let value: f32 = if record[1].is_empty() {
-            0.0
-        } else {
-            record[1].parse()?
-        };
+        let value: f32 = record[1].parse().unwrap_or_default();
 
-        model.coefs.get(&key).iter().for_each(|&w| age += w * value);
+        for (&model, age) in models.iter().zip(ages.iter_mut()) {
+            model
+                .coefs
+                .get(&key)
+                .iter()
+                .for_each(|&w| *age += w * value);
+        }
     }
 
-    Ok((model.adjust)(age))
+    for (model, age) in models.iter().zip(ages.iter_mut()) {
+        *age = (model.adjust)(*age)
+    }
+    Ok(ages)
 }
 
 fn load_coefficients(source: &[u8]) -> Result<HashMap<String, f32>, AppErr> {
@@ -76,3 +87,5 @@ fn load_coefficients(source: &[u8]) -> Result<HashMap<String, f32>, AppErr> {
 
     Ok(cs)
 }
+
+type AppErr = Box<dyn std::error::Error>;
